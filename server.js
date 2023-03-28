01a6edafc9d8d069fb73c3cb1e7e897d4f0cb12d
Isaac Park <118407259+isaacp5454@users.noreply.github.com>
2023-03-27 17:34:46 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const api = require('./Develop/public/assets/js/notes.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();  //creating an object from express node.
+
+app.use(express.json()); // this object can use json and can use url encoded.
+app.use(express.urlencoded({ extended: true }));  
+
+app.use(express.static(__dirname + '/Develop/public')); //setting default folder to our public folder. 
+app.use('/api/notes/',api)
+
+app.get('/', (req, res) =>           //sets the homepage to index.html
+  res.sendFile(path.join(__dirname, '/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html')) //sets the notes to notes.html.
+);
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`) //listening to port.
+);
